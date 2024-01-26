e88ad6894c55fe1ea939f3608780b060e8b0ca83
Tanner Sackett <136288206+tesackett@users.noreply.github.com>
2024-01-25 23:41:02 -0600

    created ticket class

--- src/main/java/com/example/tannersackettcustomersupport/Ticket.java
+++ src/main/java/com/example/tannersackettcustomersupport/Ticket.java
@@ -1,4 +1,14 @@
 package com.example.tannersackettcustomersupport;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ticket {
-}
+    private String customerName;
+    private String subject;
+    private String body;
+    private Map<Integer, Attachment> attachments;
+
+    public Ticket() {
+        attachments = new HashMap<>();
+    }
+
